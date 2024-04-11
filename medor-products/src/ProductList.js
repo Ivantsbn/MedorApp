df64168d09f167252fe7f579f7ec3b6bee8a6e34
IvanT <113232712+Ivantsbn@users.noreply.github.com>
2024-04-11 10:40:28 +0200

    create ProductList.js

---
+++ medor-products/src/ProductList.js
@@ -0,0 +1,31 @@
+// src/ProductList.js
+import React from 'react';
+
+const ProductList = ({ products }) => {
+    return (
+        //seznam produktu
+        <div className="product-list"> 
+        <h2>Seznam produktů</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Název</th>
+                    <th>Úhrada</th>
+                    <th>Výskyt na trhu</th>
+                </tr>
+            </thead>
+            <tbody>
+                {products.map((product, index) => (
+                    <tr key={index}>
+                    <td>{product.name}</td>
+                    <td>{product.payment}</td>
+                    <td>{product.marketPresence ? 'Ano' : 'Ne'}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        </div>
+    );
+};
+
+export default ProductList;
