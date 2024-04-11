fa2d719de5247f1d29319a3e8d0ffb1b38918fe4
IvanT <113232712+Ivantsbn@users.noreply.github.com>
2024-04-11 11:05:05 +0200

    create AddProductForm component

--- medor-products/src/AddProductForm.js
+++ medor-products/src/AddProductForm.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 
+// komponenta pro pridani noveho produktu 
 const AddProductForm = ({ addProduct }) => {
+
+    //promenne pro uchovani hodnot formulare
     const [name, setName] = useState('');
     const [payment, setPayment] = useState('');
     const [marketPresence, setMarketPresnce] = useState(false);
 
-    const handlSubmit = (e) => {
+    // funkce pro odesilani formulare
+    const handleSubmit = (e) => {
         e.preventDefault();
         if (!name || !payment) return;
         addProduct({ name, payment, marketPresence});
@@ -14,5 +18,33 @@ const AddProductForm = ({ addProduct }) => {
         setMarketPresnce(false);
     };
 
-    return ()
+    return (
+        <div className="add-product-form">
+            <h2>Přídat nový product</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Název:
+                    <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
+                </label>
+                <label>
+                    Úhrada:
+                    <select value={payment} onChange={(e) => setPayment(e.target.value)}>
+                        <option value="hrazené">Hrazené</option>
+                        <option value="nehrazené">Nehrazené</option>
+                        <option value="neuvedeno">Neuvedeno</option>
+                    </select>
+                </label>
+                <label>
+                    Výskyt na trhu:
+                    <input
+                        type="checkbox"
+                        checked={marketPresence}
+                        onChange={(e) => setMarketPresence(e.target.checked)}
+                    />
+                </label>
+                <button type="submit">Přidat produkt</button>
+            </form>
+        </div>
+    )
 }
+export default AddProductForm;
